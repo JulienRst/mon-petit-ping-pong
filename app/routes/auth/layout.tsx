46515a8ba272855ba21f52d1d7383a38d495d3ce
Julien Rousset <julien.rousset01@gmail.com>
2025-05-29 23:29:18 +0200

    feat(welcome): add welcome screen

---
+++ app/routes/auth/layout.tsx
@@ -0,0 +1,27 @@
+import type { PropsWithChildren } from "react";
+import { useNavigate } from "react-router";
+import { Button } from "~/components/ui/button";
+import LogoInline from "app/images/logo_inline.png";
+
+export default function authLayout({ children }: PropsWithChildren) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex h-dvh w-full flex-col p-9">
+      <Button
+        className="absolute top-3.5 left-3.5 text-2xl"
+        variant="ghost"
+        onClick={() => navigate(-1)}
+        size="icon"
+      >
+        ←
+      </Button>
+      <img
+        className="w-full max-w-[184px] self-center"
+        src={LogoInline}
+        alt="Mon petit Ping-Pong!"
+      />
+      <div className="grow">{children}</div>
+    </div>
+  );
+}
---
+++ app/routes/welcome.tsx
@@ -0,0 +1,28 @@
+import { useTranslation } from "react-i18next";
+import Logo from "app/images/logo.png";
+import { Button } from "~/components/ui/button";
+import { Link } from "react-router";
+
+export default function Welcome() {
+  const { t } = useTranslation();
+  return (
+    <div className="flex h-dvh flex-col justify-center gap-3 pr-9 pl-9">
+      <img
+        className="w-full max-w-[245px] self-center"
+        src={Logo}
+        alt={t("common.logo.alt")}
+      />
+      <p className="text-center">{t("common.logo.baseline")}</p>
+      <div className="flex flex-col gap-6">
+        <Link className="block" to="/signin">
+          <Button className="w-full" size="lg">
+            {t("auth.signup")}
+          </Button>
+        </Link>
+        <Button size="lg" variant="secondary">
+          {t("auth.signin")}
+        </Button>
+      </div>
+    </div>
+  );
+}
